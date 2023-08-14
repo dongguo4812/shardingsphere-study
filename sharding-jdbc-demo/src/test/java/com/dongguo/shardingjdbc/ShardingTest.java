75a52e8517199792e1e10b7eb1fead5c478bcc20
dongguo <291320608@qq.com>
2023-08-14 19:58:33 +0800

    sharding测试类

---
+++ sharding-jdbc-demo/src/test/java/com/dongguo/shardingjdbc/ShardingTest.java
@@ -0,0 +1,51 @@
+package com.dongguo.shardingjdbc;
+
+import com.dongguo.shardingjdbc.entity.Order;
+import com.dongguo.shardingjdbc.entity.User;
+import com.dongguo.shardingjdbc.mapper.OrderMapper;
+import com.dongguo.shardingjdbc.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+/**
+ * @author dongguo
+ * @date 2023/8/14
+ * @description:
+ */
+@SpringBootTest
+public class ShardingTest {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 垂直分片：插入数据测试
+     */
+    @Test
+    public void testInsertOrderAndUser(){
+        User user = new User();
+        user.setUname("张三");
+        userMapper.insert(user);
+
+        Order order = new Order();
+        order.setOrderNo("SP202308140001");
+        order.setUserId(user.getId());
+        order.setAmount(new BigDecimal(100));
+        orderMapper.insert(order);
+    }
+    /**
+     * 垂直分片：查询数据测试
+     */
+    @Test
+    public void testSelectFromOrderAndUser(){
+        User user = userMapper.selectById(1L);
+        Order order = orderMapper.selectById(1L);
+        System.out.println(user);
+        System.out.println(order);
+    }
+}
