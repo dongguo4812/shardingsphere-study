15a1a2de4650ca45511bd5d759e2fe8d205debd1
dongguo <291320608@qq.com>
2023-08-17 22:14:43 +0800

    分库分表

--- sharding-jdbc-demo/src/test/java/com/dongguo/shardingjdbc/ShardingTest.java
+++ sharding-jdbc-demo/src/test/java/com/dongguo/shardingjdbc/ShardingTest.java
@@ -75,25 +75,83 @@ public class ShardingTest {
             orderMapper.insert(order);
         }
     }
+    /**
+     * 水平分片：分表插入数据测试
+     */
+    @Test
+    public void testInsertOrderTableStrategy(){
+        for (long i = 1; i <= 10; i++) {
+            Order order = new Order();
+            order.setId(i);
+            order.setOrderNo("SP20230814000" + i);
+            order.setUserId(i);
+            order.setAmount(new BigDecimal(100));
+            orderMapper.insert(order);
+        }
+    }
 
+    /**
+     * 水平分片：分库分表插入数据
+     */
+    @Test
+    public void testInsertOrderDatabaseAndTableStrategy(){
+        for (long i = 1; i < 5; i++) {
 
+            Order order = new Order();
+            order.setId(i);
+            order.setOrderNo("SP20230814000" + i);
+            order.setUserId(1L);
+            order.setAmount(new BigDecimal(100));
+            orderMapper.insert(order);
+        }
 
+        for (long i = 5; i < 9; i++) {
+            Order order = new Order();
+            order.setId(i);
+            order.setOrderNo("SP20230814000" + i);
+            order.setUserId(2L);
+            order.setAmount(new BigDecimal(100));
+            orderMapper.insert(order);
+        }
+    }
+
+    /**
+     * 自动分片创建数据库表
+     */
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Test
     public void testCreateAutoOrderMod() {
         jdbcTemplate.execute("CREATE TABLE t_order (\n" +
-                "  order_id BIGINT,\n" +
+                "  id BIGINT,\n" +
                 "  order_no VARCHAR(30),\n" +
                 "  user_id BIGINT,\n" +
                 "  amount DECIMAL(10,2),\n" +
-                "  PRIMARY KEY(order_id) USING BTREE\n" +
+                "  PRIMARY KEY(id) USING BTREE\n" +
                 ") ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;");
-        for (long i = 1; i <= 10; i++) {
+    }
+
+    /**
+     * 分表插入数据测试
+     */
+    @Test
+    public void testInsertOrderShardingStrategy(){
+
+        for (long i = 1; i < 5; i++) {
+
             Order order = new Order();
             order.setOrderNo("SP20230814000" + i);
-            order.setUserId(i);
+            order.setUserId(1L);
+            order.setAmount(new BigDecimal(100));
+            orderMapper.insert(order);
+        }
+
+        for (long i = 5; i < 9; i++) {
+
+            Order order = new Order();
+            order.setOrderNo("SP20230814000" + i);
+            order.setUserId(2L);
             order.setAmount(new BigDecimal(100));
             orderMapper.insert(order);
         }
